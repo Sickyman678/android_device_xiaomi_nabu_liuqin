a699f22f6b8c826b0d7f2c1814a68f90f72bcd8e
Sickyman678 <97682657+Sickyman678@users.noreply.github.com>
2023-09-09 16:15:33 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_nabu.mk
-
-COMMON_LUNCH_CHOICES := \
-    evolution_nabu-user \
-    evolution_nabu-userdebug \
-    evolution_nabu-eng
+# 
+ # Copyright (C) 2023 The Evolution X Project 
+ # 
+ # SPDX-License-Identifier: Apache-2.0 
+ # 
+  
+ PRODUCT_MAKEFILES := \ 
+     $(LOCAL_DIR)/evolution_liuqin.mk 
+  
+ COMMON_LUNCH_CHOICES := \ 
+     evolution_liuqin-user \ 
+     evolution_liuqin-userdebug \ 
+     evolution_liuqin-eng
